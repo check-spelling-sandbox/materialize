14b0c4914eac6d8b4f3bde26d0445614f80be33c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-11-23 20:34:35 -0500

    spelling: unique
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/parallel-benchmark/mzcompose.py
+++ test/parallel-benchmark/mzcompose.py
@@ -356,9 +356,9 @@ def report(
         for key, m in measurements.data.items():
             durations = [x.duration * 1000.0 for x in m]
             durations.sort()
-            (uniqu_durations, counts) = numpy.unique(durations, return_counts=True)
+            (unique_durations, counts) = numpy.unique(durations, return_counts=True)
             counts = numpy.cumsum(counts)
-            plt.plot(uniqu_durations, 1 - counts / counts.max(), label=key)
+            plt.plot(unique_durations, 1 - counts / counts.max(), label=key)
         plt.legend(loc="best")
 
         plot_path = f"plots/{scenario_name}_{suffix}_ccdf.png"
